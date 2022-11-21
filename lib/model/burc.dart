710dd54240985af583aab7ed9161dda125d6ec37
Selin Güngör Yazıcı <selingungor90@icloud.com>
2022-11-21 10:26:58 +0300

    model class has created

--- lib/model/burc.dart
+++ lib/model/burc.dart
@@ -1,6 +1,9 @@
 class Burc {
   final String _burcAdi;
   final String _burcTarihi;
-  final String _burcDetay;
-  
+  final String _burcDetayi;
+  final String _burcKucukResim;
+  final String _burcBuyukResim;
+
+  Burc(this._burcAdi, this._burcTarihi, this._burcDetayi,this._burcKucukResim,this._burcBuyukResim);
 }
