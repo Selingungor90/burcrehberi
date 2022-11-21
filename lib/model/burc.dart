4bdbec5747be25fa326d6809a5d762aab63e848c
Selin Güngör Yazıcı <selingungor90@icloud.com>
2022-11-21 11:45:20 +0300

    db is created

--- lib/model/burc.dart
+++ lib/model/burc.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 class Burc {
   final String _burcAdi;
   final String _burcTarihi;
@@ -5,5 +7,11 @@ class Burc {
   final String _burcKucukResim;
   final String _burcBuyukResim;
 
-  Burc(this._burcAdi, this._burcTarihi, this._burcDetayi,this._burcKucukResim,this._burcBuyukResim);
+  Burc(this._burcAdi, this._burcTarihi, this._burcDetayi, this._burcKucukResim,
+      this._burcBuyukResim);
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '$_burcAdi - $_burcBuyukResim';
+  }
 }
